0287d9e4fa850840b5e8d2e6bb91c0cbd695c6cc
cobuma <marquardt.corey@gmail.com>
2015-12-28 18:24:41 -0600

    add header, grid, and sketch function

--- sketch.js
+++ sketch.js
@@ -1,12 +1,23 @@
 var dim = 16;
 
 $(document).ready(function() {
-	$("body").prepend()
+	$("body").append("<div id=container></div>");
 	create_grid(dim);
+	$("td").mouseenter(function() {
+		$(this).addClass("entered");
+	});
 });
 
-function create_grid(dim){
-	for (var i = 0; i < dim; i++) {
-		Things[i]
+function create_grid(num){
+	$("#container").prepend("<table></table>")
+	for (var i = 0; i < num; i++) {
+		$("table").append("<tr class='row"+i+"'>");
+		for (var j = 0; j < num; j++) {
+			$(".row"+i).append("<td></td>");
+		};
 	};
+}
+
+function sketch(){
+	mou
 }
